35f351aa52591e9e0cc32d1809bce39e763b843a
Der97stuganova <aderstuganova@gmail.com>
2021-09-06 01:19:07 +0300

    new commit

--- javaQA/src/main/java/andersen/App.java
+++ javaQA/src/main/java/andersen/App.java
@@ -1,13 +1,12 @@
 package andersen;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 }
+
+
+
